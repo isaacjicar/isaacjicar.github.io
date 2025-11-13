1b60188af0ad2157c89af02b463e8aa8283470c0
isaacjicar <isaacjiemmenez@gmail.com>
2025-11-13 07:22:44 -0600

    se agregan cambios

--- portafolio2025/src/module/Home/Components/projects/FeatureCard.jsx
+++ portafolio2025/src/module/Home/Components/projects/FeatureCard.jsx
@@ -1,4 +1,6 @@
-export default function FeatureCard({ title, desc, icon, href }) {
+import { Link } from "react-router-dom";
+
+export default function FeatureCard({ title, desc, icon, to }) {
   return (
     <div className="card bg-neutral/60 backdrop-blur border border-white/5 shadow-xl rounded-3xl">
       <div className="card-body p-7 md:p-9 gap-3">
@@ -6,12 +8,9 @@ export default function FeatureCard({ title, desc, icon, href }) {
         <h3 className="card-title text-secondary">{title}</h3>
         <p className="text-white/80">{desc}</p>
         <div className="card-actions justify-end pt-2">
-          <a
-            href={href}
-            className="btn btn-sm btn-secondary rounded-xl"
-          >
+          <Link to={to} className="btn btn-sm btn-secondary rounded-xl">
             Ver más
-          </a>
+          </Link>
         </div>
       </div>
     </div>
