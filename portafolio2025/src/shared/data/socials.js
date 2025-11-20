fa806ed9374638c469fb42770be25c8e53b9098b
isaacjicar <isaacjiemmenez@gmail.com>
2025-11-19 22:55:03 -0600

    se hacen cambios para que funcione correctamente el workFlow

--- portafolio2025/src/shared/data/socials.js
+++ portafolio2025/src/shared/data/socials.js
@@ -1,5 +1,4 @@
-
-import { FaLinkedinIn, FaGithub, FaEnvelope } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaEnvelope,FaWhatsapp } from "react-icons/fa";
 
 export const defaultSocials = [
   {
@@ -7,20 +6,29 @@ export const defaultSocials = [
     label: "LinkedIn",
     href: "https://www.linkedin.com/in/isaac-jimenez-a5b143197",
     icon: FaLinkedinIn,
-    color: "#4C9FB3", // secundario
+    color: "#4C9FB3", 
   },
+  
   {
     id: "github",
     label: "GitHub",
     href: "https://github.com/isaacjicar",
     icon: FaGithub,
-    color: "#4CB393", // acento
+    color: "#4CB393", 
   },
+
   {
-    id: "email",
-    label: "Correo",
+    id: "correo",
+    label: "Email",
     href: "mailto:isaacjiemmenez@gmail.com",
     icon: FaEnvelope,
-    color: "#4C6CB3", // primario
+    color: "#4C6CB3", 
   },
+  {
+    id: "Numero",
+    label: "Whatsapp",
+    href: "https://wa.me/50663270092?",
+    icon: FaWhatsapp,
+    color: "#4C6C48", //
+  }
 ];
